2ea6a0d7c4a0dbe8a365f54f4948c80448fa7c7f
Lucas Peres <lucasperesss@gmail.com>
2023-04-07 23:23:13 -0300

    feat: Inicializando o projeto com o Menu

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import PersonIcon from "@mui/icons-material/Person";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import SettingsIcon from "@mui/icons-material/Settings";
+import ShortcutIcon from "@mui/icons-material/Shortcut";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+import "./styles.scss";
+
+export default function Menu() {
+  const [selectedSection, setSelectedSection] = useState("home");
+
+  const onClick = (section) => setSelectedSection(section);
+
+  return (
+    <div className="menu">
+      <header>
+        <div className="banner" />
+        <div className="game-points">
+          <EmojiEventsIcon />
+          <div className="points-container">
+            <div className="points-counter">60/100</div>
+            <div className="points-bar">
+              <div className="points" style={{ width: "60%" }} />
+            </div>
+          </div>
+        </div>
+      </header>
+      <nav>
+        <button
+          type="button"
+          onClick={() => onClick("home")}
+          className={`nav-btn ${
+            selectedSection === "home" ? "selected-section" : "not-selected"
+          }`}
+        >
+          <HomeIcon />
+          Home
+        </button>
+        <button
+          type="button"
+          onClick={() => onClick("members")}
+          className={`nav-btn ${
+            selectedSection === "members" ? "selected-section" : "not-selected"
+          }`}
+        >
+          <PersonIcon />
+          Membros
+        </button>
+        <button
+          type="button"
+          onClick={() => onClick("game")}
+          className={`nav-btn ${
+            selectedSection === "game" ? "selected-section" : "not-selected"
+          }`}
+        >
+          <SportsEsportsIcon />
+          Gamificação
+        </button>
+        <button
+          type="button"
+          onClick={() => onClick("adm")}
+          className={`nav-btn ${
+            selectedSection === "adm" ? "selected-section" : "not-selected"
+          }`}
+        >
+          <SettingsIcon />
+          Administração
+        </button>
+        <button
+          type="button"
+          onClick={() => onClick("invite")}
+          className={`nav-btn ${
+            selectedSection === "invite" ? "selected-section" : "not-selected"
+          }`}
+        >
+          <ShortcutIcon />
+          Convide Membros
+        </button>
+      </nav>
+    </div>
+  );
+}
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Menu from "../../components/Menu/Menu";
+import "./styles.scss";
+
+export default function Home() {
+  return (
+    <div>
+      <Menu />
+    </div>
+  );
+}
